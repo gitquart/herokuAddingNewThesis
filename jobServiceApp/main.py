c19a405900032f117fba2e033a41b2ce981099d0
Ulyses quart <ulyses@quart.com.mx>
2020-10-03 10:05:34 -0500

    ...

--- jobServiceApp/main.py
+++ jobServiceApp/main.py
@@ -64,7 +64,7 @@ browser=webdriver.Chrome()
 def main():
     print('Running program...')
     #The limits in readUrl may vary up to the need of the search
-    res=readUrl(1,2022136,2500000)  
+    res=readUrl(1,2022149,2500000)  
     print("Main program is done")
   
   
